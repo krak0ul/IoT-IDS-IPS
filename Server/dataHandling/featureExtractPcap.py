96eace82e76c327362eaf904b98e201f3eedd243
krak0ul <103131726+krak0ul@users.noreply.github.com>
2025-03-10 21:54:58 +0100

    settings file (globals) for dataHandling

--- Server/dataHandling/featureExtractPcap.py
+++ Server/dataHandling/featureExtractPcap.py
@@ -2,11 +2,21 @@ import sys
 import pyshark as ps
 import pandas as pd
 
+<<<<<<< HEAD
 
 file_name = 'pcaps/test.pcap'
 features = ['arp.opcode', 'arp.hw.size', 'icmp.checksum', 'icmp.seq_le', 'icmp.unused', 'http.content_length', 'http.request.method', 'http.referer', 'http.request.version', 'http.response', 'http.tls_port', 'tcp.ack', 'tcp.ack_raw', 'tcp.checksum', 'tcp.connection.fin', 'tcp.connection.rst', 'tcp.connection.syn', 'tcp.connection.synack', 'tcp.flags', 'tcp.flags.ack', 'tcp.len', 'tcp.seq', 'udp.stream', 'udp.time_delta', 'dns.qry.name', 'dns.qry.name.len', 'dns.qry.qu', 'dns.qry.type', 'dns.retransmission', 'dns.retransmit_request', 'dns.retransmit_request_in', 'mqtt.conack.flags', 'mqtt.conflag.cleansess', 'mqtt.conflags', 'mqtt.hdrflags', 'mqtt.len', 'mqtt.msg_decoded_as', 'mqtt.msgtype', 'mqtt.proto_len', 'mqtt.protoname', 'mqtt.topic', 'mqtt.topic_len', 'mqtt.ver', 'mbtcp.len', 'mbtcp.trans_id', 'mbtcp.unit_id']
 
 
+=======
+from settings import PCAP_FILE, FEATURES
+
+
+file_name = PCAP_FILE
+features = FEATURES
+
+
+>>>>>>> 6f56791 (settings file (globals) for dataHandling)
 def open_pcap(file_name):
     print('Opening {}...'.format(file_name))
     return ps.FileCapture(input_file=file_name)
