5a978b93255317c8e0f6cdd2aa7cfad1fa95292e
krak0ul <103131726+krak0ul@users.noreply.github.com>
2025-03-10 21:21:16 +0100

    cleaned up feature extractor

--- Server/dataHandling/featureExtractPcap.py
+++ Server/dataHandling/featureExtractPcap.py
@@ -1,35 +1,40 @@
 import sys
-from scapy.all import rdpcap, IPv6
 import pyshark as ps
 import pandas as pd
 
 
 file_name = 'pcaps/test.pcap'
+features = ['arp.opcode', 'arp.hw.size', 'icmp.checksum', 'icmp.seq_le', 'icmp.unused', 'http.content_length', 'http.request.method', 'http.referer', 'http.request.version', 'http.response', 'http.tls_port', 'tcp.ack', 'tcp.ack_raw', 'tcp.checksum', 'tcp.connection.fin', 'tcp.connection.rst', 'tcp.connection.syn', 'tcp.connection.synack', 'tcp.flags', 'tcp.flags.ack', 'tcp.len', 'tcp.seq', 'udp.stream', 'udp.time_delta', 'dns.qry.name', 'dns.qry.name.len', 'dns.qry.qu', 'dns.qry.type', 'dns.retransmission', 'dns.retransmit_request', 'dns.retransmit_request_in', 'mqtt.conack.flags', 'mqtt.conflag.cleansess', 'mqtt.conflags', 'mqtt.hdrflags', 'mqtt.len', 'mqtt.msg_decoded_as', 'mqtt.msgtype', 'mqtt.proto_len', 'mqtt.protoname', 'mqtt.topic', 'mqtt.topic_len', 'mqtt.ver', 'mbtcp.len', 'mbtcp.trans_id', 'mbtcp.unit_id']
 
 
-def process_pcap(file_name):
+def open_pcap(file_name):
     print('Opening {}...'.format(file_name))
-    packets = ps.FileCapture(input_file=file_name)
+    return ps.FileCapture(input_file=file_name)
 
+def filter_packets(packets):
+    filtered_pkts = []
     for pkt in packets:
-        # discard IPv6 packets
+        # only keep packets that have an ethernet layer
         if (hasattr(pkt, 'eth')):
+            # discard IPv6 packets
             if (pkt.eth.type == '0x86dd'):
-                print('IPV6 PACKET')
+                # print('IPV6 PACKET')
                 pass
 
             else:
-                pkt.pretty_print()
-                feature_extraction(pkt)
+                # pkt.pretty_print()
+                filtered_pkts.append(pkt)
+                # print(pkt)
         else:
             pass
+    # print(filtered_pkts)
+    return filtered_pkts
         
 
-def feature_extraction(pkt):
+def feature_extraction(pkt, features):
     """
     Extract features from a raw packet. The features is the same we trained our model with.
     """
-    features = ['arp.opcode', 'arp.hw.size', 'icmp.checksum', 'icmp.seq_le', 'icmp.unused', 'http.content_length', 'http.request.method', 'http.referer', 'http.request.version', 'http.response', 'http.tls_port', 'tcp.ack', 'tcp.ack_raw', 'tcp.checksum', 'tcp.connection.fin', 'tcp.connection.rst', 'tcp.connection.syn', 'tcp.connection.synack', 'tcp.flags', 'tcp.flags.ack', 'tcp.len', 'tcp.seq', 'udp.stream', 'udp.time_delta', 'dns.qry.name', 'dns.qry.name.len', 'dns.qry.qu', 'dns.qry.type', 'dns.retransmission', 'dns.retransmit_request', 'dns.retransmit_request_in', 'mqtt.conack.flags', 'mqtt.conflag.cleansess', 'mqtt.conflags', 'mqtt.hdrflags', 'mqtt.len', 'mqtt.msg_decoded_as', 'mqtt.msgtype', 'mqtt.proto_len', 'mqtt.protoname', 'mqtt.topic', 'mqtt.topic_len', 'mqtt.ver', 'mbtcp.len', 'mbtcp.trans_id', 'mbtcp.unit_id']
     pkt_features = []
 
     for feature in features:
@@ -37,10 +42,9 @@ def feature_extraction(pkt):
         feature_value = get_attr(pkt, feature)
         pkt_features.append(feature_value)
 
-    print(pkt_features)
-    pkt_features_list = [pkt_features]      # needs to be a nested list to go in the dataFrame
-    df = pd.DataFrame(data=[pkt_features], columns=features)
-    print(df.head())
+    # print(pkt_features)
+    return pkt_features
+    
 
 
 def get_attr(pkt, attr_str):
@@ -60,14 +64,23 @@ def get_attr(pkt, attr_str):
         try: 
             # dirty try - except is temporary solution because some protocols don't always use all attributes
             attr = getattr(pkt, fields[0])._all_fields[attr_str]
-            print(attr_str + ': ' + str(attr))
+            # print(attr_str + ': ' + str(attr))
             return attr        
         except:
             return 0
 
-    
 
 
 if __name__ == '__main__':
-    process_pcap(file_name)
+    packets = open_pcap(file_name)
+    filtered_pkts = filter_packets(packets)
+    
+    pkt_features_list = [] 
+
+    for pkt in filtered_pkts:
+        pkt_features = feature_extraction(pkt, features)
+        pkt_features_list.append(pkt_features)
+        
+    df = pd.DataFrame(data=pkt_features_list, columns=features)
+    print(df)
     sys.exit(0)
