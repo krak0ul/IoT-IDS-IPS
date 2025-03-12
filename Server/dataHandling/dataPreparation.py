b452aab3e9447a291f2b3a20649dbd95ab679822
krak0ul <103131726+krak0ul@users.noreply.github.com>
2025-03-12 19:50:40 +0100

    implemented data preparation functions

--- Server/dataHandling/dataPreparation.py
+++ Server/dataHandling/dataPreparation.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
 
 def cleanValues(df):
+    df = df.copy()  # Ensure we're working on a copy
+
     df = df.drop_duplicates()
 
     # Replace 'INF' or '-INF' with NaN (if they exist)
@@ -12,7 +16,7 @@ def cleanValues(df):
 
     return df
 
-def label_encode(df):
+def labelEncode(df):
     # Identify categorical features
     categorical_features = df.select_dtypes(include=['object']).columns
 
@@ -20,8 +24,24 @@ def label_encode(df):
     label_encoders = {}
     for col in categorical_features:
         le = LabelEncoder()
+        
+        df[col] = df[col].astype(str)   # Convert column values to strings to ensure uniformity
         df[col] = le.fit_transform(df[col])
         label_encoders[col] = le
 
     return df
     
+def scaleFeatures(df):
+    # Standardize numerical features
+    scaler = StandardScaler()
+    scaled_columns = df.columns
+    df[scaled_columns] = scaler.fit_transform(df[scaled_columns])
+
+    print("Feature scaling applied.")
+    return df
+
+def prepareData(df):
+    df = cleanValues(df)
+    df = labelEncode(df)
+    df = scaleFeatures(df)
+    return df
--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,8 @@
 import sys
 
-from dataHandling.featureExtractPcap import pcap_to_df
 from settings import PCAP_FILE, FEATURES, MODEL
+from dataHandling.featureExtractPcap import pcap_to_df
+from dataHandling.dataPreparation import prepareData
 
 
 file_name = PCAP_FILE
@@ -11,6 +12,8 @@ model_pickle = MODEL
 
 if __name__ == '__main__':
         df = pcap_to_df(file_name, features)
+        print(df)
+        df = prepareData(df)
         print(df)
         
         sys.exit(0)
