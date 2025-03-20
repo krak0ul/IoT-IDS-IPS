44efb29b05321e104447fb3e0c26bbfefa6293cd
krak0ul <103131726+krak0ul@users.noreply.github.com>
2025-03-20 15:38:22 +0100

    fixed model parameters in settings

--- server/settings.py
+++ server/settings.py
@@ -10,24 +10,8 @@ SCALER = "server/pickles/scaler.pkl"
 
 # Features to extract from packets
 # Adjust these to match the features your model was trained on
-FEATURES = [
-    'frame.len',
-    'ip.len',
-    'ip.flags',
-    'ip.ttl',
-    'tcp.len',
-    'tcp.ack',
-    'tcp.flags',
-    'tcp.window_size',
-    'udp.length',
-    'http.request.method',
-    'http.referer',
-    'http.request.version',
-    'dns.qry.name.len',
-    'mqtt.conack.flags',
-    'mqtt.protoname',
-    'mqtt.topic'
-]
+FEATURES = ['arp.opcode', 'arp.hw.size', 'icmp.checksum', 'icmp.seq_le', 'icmp.unused', 'http.content_length', 'http.request.method', 'http.referer', 'http.request.version', 'http.response', 'http.tls_port', 'tcp.ack', 'tcp.ack_raw', 'tcp.checksum', 'tcp.connection.fin', 'tcp.connection.rst', 'tcp.connection.syn', 'tcp.connection.synack', 'tcp.flags', 'tcp.flags.ack', 'tcp.len', 'tcp.seq', 'udp.stream', 'udp.time_delta', 'dns.qry.name', 'dns.qry.name.len', 'dns.qry.qu', 'dns.qry.type', 'dns.retransmission', 'dns.retransmit_request', 'dns.retransmit_request_in', 'mqtt.conack.flags', 'mqtt.conflag.cleansess', 'mqtt.conflags', 'mqtt.hdrflags', 'mqtt.len', 'mqtt.msg_decoded_as', 'mqtt.msgtype', 'mqtt.proto_len', 'mqtt.protoname', 'mqtt.topic', 'mqtt.topic_len', 'mqtt.ver', 'mbtcp.len', 'mbtcp.trans_id', 'mbtcp.unit_id']
+
 
 # Network settings
 HOST = 'localhost'  # Listen on all interfaces
